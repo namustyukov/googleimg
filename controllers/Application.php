3f4b4d60c4ce57975710af5bae456a8df64aff0f
namustyukov <256nail@mail.ru>
2017-12-26 23:27:40 +0500

    get links from google images

--- controllers/Application.php
+++ controllers/Application.php
@@ -1,5 +1,7 @@
 <?php
 
+require __DIR__.'/GoogleImageSearch.php';
+
 class Application
 {
     private $_files = [];
@@ -68,6 +70,27 @@ class Application
 
     private function _googleRequest($file)
     {
+        $imageSearch = new GoogleImageSearch();
+
+        echo "Search by image URL: <br />\n";
+        if($results = $imageSearch->search('http://upload.wikimedia.org/wikipedia/commons/2/22/Turkish_Van_Cat.jpg', 2)) {
+            if($results['search_results']) {
+                echo "Best guess: <strong><a href=\"{$results['best_guess'][1]}\">{$results['best_guess'][0]}</strong><br />\n";
+                echo "<ol><br />\n";
+                foreach($results['search_results'] as $k => $r) {
+                    echo "<li><a href=\"{$r[1]}\">{$r[0]}</a> ; <a href=\"{$r[2]}\">Original image</a></li>\n";
+                }
+                echo "</ol><br />\n";
+            } else {
+                echo 'Nothing found';
+            }
+
+        }
+        echo "Search by uploading local image: <br />\n";
+
+        exit();
+
+
         return [];
     }
 
