bc308d6c88d09e08ac8f5304a1759db05ab0af7d
mustyukov <nail.mustyukov@walletone.com>
2017-12-28 11:30:21 +0500

    fix view

--- views/index.php
+++ views/index.php
@@ -39,16 +39,27 @@
                 foreach ($files as $file) {
                 ?>
                     <div class="result">
-                        <div class="result_header"><?= $file->name ?> <span>[<?= $pages ?> pages]</span></div>
-                    <?php
-                        if ($file->result) {
-                            foreach ($file->result as $result) {
-                                echo '<div class="result_row">'.$result.'</div>';
+                        <div class="result_header">
+                            <div class="result_header_right">
+                                <span class="btn copy-btn">Copy links</span>
+                            </div>
+                            <div class="result_header_left">
+                                <img src="/img/<?= $file->name ?>">
+                                <?= $file->name ?>
+                                <span>[<?= $pages ?> pages]</span>
+                            </div>
+                        </div>
+                        <div class="result_list">
+                        <?php
+                            if ($file->result) {
+                                foreach ($file->result as $result) {
+                                    echo '<div class="result_row">'.$result.'</div>';
+                                }
+                            } else {
+                                echo '<div class="empty">No result</div>';
                             }
-                        } else {
-                            echo '<div class="empty">No result</div>';
-                        }
-                    ?>
+                        ?>
+                        </div>
                     </div>
                 <?php
                 }
@@ -57,5 +68,33 @@
             }
         ?>
     </div>
+
+<script>
+    var copyBtn = document.querySelectorAll('.copy-btn');
+    copyBtn.forEach(function(btn) {
+        btn.addEventListener('click', handler);
+    });
+
+    function handler(event) {
+        // Select result list
+        var list = event.target.parentNode.parentNode.parentNode.querySelector('.result_list');
+        var range = document.createRange();
+        range.selectNode(list);
+        window.getSelection().addRange(range);
+
+        try {
+            // Run copy
+            var successful = document.execCommand('copy');
+            var msg = successful ? 'Successful copy' : 'Error: not copy';
+        } catch(err) {
+            var msg = 'Error: unable to copy';
+        }
+
+        // Reset range
+        window.getSelection().removeAllRanges();
+
+        alert(msg);
+    }
+</script>
 </body>
 </html>
